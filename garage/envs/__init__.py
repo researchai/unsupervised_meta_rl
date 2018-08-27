306360ed51bea22b3a4964fdf5d941708b0beecd
zhanpenghe <zhanpenh@usc.edu>
2018-08-27 23:04:52 +0000

    no mujoco

--- garage/envs/__init__.py
+++ garage/envs/__init__.py
@@ -1,11 +1,11 @@
 from garage.envs.base import GarageEnv
 from garage.envs.base import Step
 from garage.envs.env_spec import EnvSpec
-from garage.envs.grid_world_env import GridWorldEnv
-from garage.envs.identification_env import IdentificationEnv  # noqa: I100
-from garage.envs.noisy_env import DelayedActionEnv
-from garage.envs.noisy_env import NoisyObservationEnv
-from garage.envs.normalized_env import normalize
-from garage.envs.normalized_env import NormalizedEnv
-from garage.envs.occlusion_env import OcclusionEnv
-from garage.envs.sliding_mem_env import SlidingMemEnv
+# from garage.envs.grid_world_env import GridWorldEnv
+# from garage.envs.identification_env import IdentificationEnv  # noqa: I100
+# from garage.envs.noisy_env import DelayedActionEnv
+# from garage.envs.noisy_env import NoisyObservationEnv
+# from garage.envs.normalized_env import normalize
+# from garage.envs.normalized_env import NormalizedEnv
+# from garage.envs.occlusion_env import OcclusionEnv
+# from garage.envs.sliding_mem_env import SlidingMemEnv
