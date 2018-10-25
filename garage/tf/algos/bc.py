654449c7c55bbf51cd89419b44c433d5aea175b1
Aravind Kumaraguru <5566152+akumaraguru@users.noreply.github.com>
2018-10-25 16:51:27 -0700

    Added function to sample from expert policy for BC

--- garage/tf/algos/bc.py
+++ garage/tf/algos/bc.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 from garage.misc import logger
 from garage.misc.overrides import overrides
+from garage.replay_buffer import SimpleReplayBuffer
 from garage.tf.algos import RLAlgorithm
 from garage.tf.misc import tensor_utils
 from garage.tf.plotter import Plotter
@@ -13,7 +14,7 @@ class BC(RLAlgorithm):
                  env,
                  policy,
                  expert_dataset,
-                 policy_lr=1e-4,
+                 policy_lr=1e-3,
                  policy_optimizer=tf.train.AdamOptimizer,
                  plot=False,
                  pause_for_plot=False,
@@ -65,9 +66,8 @@ class BC(RLAlgorithm):
                     name="input_expert_action")
 
             with tf.name_scope("action_loss"):
-                # TODO: Does MSE work for BC?
                 action_loss = tf.reduce_mean(
-                    tf.square(actions - expert_actions))
+                    tf.squared_difference(actions, expert_actions))
 
             with tf.name_scope("minimize_action_loss"):
                 policy_train_op = self.policy_optimizer(
@@ -91,23 +91,23 @@ class BC(RLAlgorithm):
     def obtain_samples(self, itr):
         """Select a batch of (obs,act) pairs from the expert. Then sample target with obs"""
         expert_batch = self.expert_dataset.sample(self.buffer_batch_size)
-        assert "actions" in expert_batch
-        assert "observations" in expert_batch
+        assert "action" in expert_batch
+        assert "observation" in expert_batch
 
         # TODO: Calculate expert and target actions' reward
         if self.policy.vectorized:
             target_actions, _ = self.policy.get_actions(
-                expert_batch["observations"])
+                expert_batch["observation"])
         else:
             target_actions = [
                 self.policy.get_action(exp_obs)
-                for exp_obs in expert_batch["observations"]
+                for exp_obs in expert_batch["observation"]
             ]
             target_actions = np.array(target_actions)
 
         return dict(
-            observations=expert_batch["observations"],
-            expert_actions=expert_batch["actions"],
+            observations=expert_batch["observation"],
+            expert_actions=expert_batch["action"],
             target_actions=target_actions,
         )
 
@@ -178,3 +178,52 @@ class BC(RLAlgorithm):
         if created_session:
             sess.close()
         return last_average_return
+
+
+def sample_expert_policy_to_dataset(env,
+                                    policy,
+                                    tf_session_pkl,
+                                    size_in_transitions=800,
+                                    time_horizon=200):
+    buffer_shapes = {
+        "action": env.action_space.shape,
+        "observation": env.observation_space.shape
+    }
+    replay_buffer = SimpleReplayBuffer(
+        env_spec=env.spec,
+        size_in_transitions=size_in_transitions,
+        time_horizon=time_horizon)
+
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        expert_tf_session = (tf_session_pkl)
+        saver.restore(sess, expert_tf_session)
+        while not replay_buffer.full:
+            done = False
+            obs = env.reset()
+            samples = 0
+            while not done and (not replay_buffer.full):
+                action, _ = policy.get_action(obs)
+                # To visualize how our clone model behaves, make a
+                # prediction from the clone and step based on the retrieved
+                # action.
+                next_obs, reward, done, info = env.step(action)
+                samples += 1
+                replay_buffer.add_transition(
+                    action=[action],
+                    observation=[obs],
+                    terminal=[done],
+                    reward=[reward],
+                    next_observation=[next_obs])
+                obs = next_obs
+            print("Samples collected by episode in the expert", samples)
+        env.close()
+
+    data = replay_buffer.sample(size_in_transitions - 1)
+    np.save("garage/tf/behavioral_cloning/expert_data.npy", data)
+    # Retrieve data as:
+    # expert = np.load("garage/tf/behavioral_cloning/expert_data.npy")
+    # obs = expert[()]["observation"]
+    # acts = expert[()]["action"]
+
+    return replay_buffer
