021f0e2bd3216a07f2ad066dc9c2f2b0bd1d7b6f
Angel Gonzalez <aigonzal@usc.edu>
2018-11-19 13:56:49 -0800

    Add sequential sampling for point experts
    Arguments have been added to the python script sample_point_expert to
    sample randomly or sequentially. Execute the script to see the argument
    options.
    The details of the sequential sampling and how to retrieve the data are
    described in the docstring of function sample_from_experts_sequential.

--- garage/tf/behavioral_cloning/sample_point_expert.py
+++ garage/tf/behavioral_cloning/sample_point_expert.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import tensorflow as tf
 
@@ -8,7 +10,8 @@ from garage.tf.envs import TfEnv
 from garage.tf.policies import GaussianMLPPolicy
 
 
-def sample_from_experts(tasks, time_horizon=50, size_in_transitions=800):
+def sample_from_experts_random(tasks, time_horizon=50,
+                               size_in_transitions=800):
     """Sample actions and observations from the expert
 
     We load the TensorFlow variables that define our expert from the TF session
@@ -74,10 +77,116 @@ def sample_from_experts(tasks, time_horizon=50, size_in_transitions=800):
             # acts = expert[()]["action"]
 
 
-def main():
+def sample_from_experts_sequential(tasks, num_trajectories=50,
+                                   time_horizon=50):
+    """Sample actions and observations from the expert
+
+    We load the TensorFlow variables that define our expert from the TF session
+    stored in the folder "ppo_point_expert", and sample actions, rewards, done
+    states and observations from it, saving the results in a numpy structured
+    array and in the numpy file at
+    "garage/tf/behavioral_cloning/point_data_sequential.npy".
+    The data is divided by a task ID, and each task is divided by trajectories.
+    Each trajectory contains a sequence of tuples with values (action,
+    observation, terminal, reward, next_observation).
+    Follow the next example to retrieve the data from the saved filed:
+    ```
+    import numpy as np
+
+    data = np.load("garage/tf/behavioral_cloning/point_data_sequential.npy")
+    task_1 = data.item().get("task_1")
+    trajectory = task_1[0]  # Get the first trajectory from task 1
+    print(trajectory["observation"])
+    ```
+
+
+    Parameters:
+        - tasks: dictionary of task IDs (must match the id in the TF session)
+          and tuples of goals (coordinate in two dimensional space)
+        - num_trajectories: number of all trajectories sampled. This number is
+          divided as evenly as possible amongst all tasks. For example, if the
+          number of trajectories is 50 and there are four tasks, then 13
+          trajectories will sampled for 2 tasks, and 12 trajectories for the
+          remaining 2.
+        - time_horizon: maximum number of transitions per trajectory
+    """
+
+    num_tasks = len(tasks.items())
+    remaining_trajectories = num_trajectories % num_tasks
+
+    all_tasks = {}
+    for task_id, task in tasks.items():
+        with tf.Graph().as_default():
+            env = TfEnv(normalize(PointEnv(goal=task)))
+            dt = np.dtype(
+                [("action", env.spec.action_space.low.dtype,
+                  env.spec.action_space.shape),
+                 ("observation", env.spec.observation_space.low.dtype,
+                  env.spec.observation_space.shape), ("terminal", np.bool),
+                 ("reward", np.float32), ("next_observation",
+                                          env.spec.observation_space.low.dtype,
+                                          env.spec.observation_space.shape)])
+
+            policy = GaussianMLPPolicy(
+                name="policy",
+                env_spec=env.spec,
+                hidden_sizes=(20, 20),
+                hidden_nonlinearity=tf.nn.relu)
+
+            with tf.Session() as sess:
+                saver = tf.train.Saver()
+                saver.restore(
+                    sess, "./garage/tf/behavioral_cloning/" +
+                    "ppo_point_expert/ppo_task_" + task_id + ".ckpt")
+
+                num_trajectories_per_task = int(num_trajectories / num_tasks)
+                if remaining_trajectories:
+                    num_trajectories_per_task += 1
+                    remaining_trajectories -= 1
+
+                completed_trajec = 0
+                trajec_per_task = np.zeros(
+                    (num_trajectories_per_task, time_horizon), dtype=dt)
+
+                while completed_trajec != num_trajectories_per_task:
+                    done = False
+                    obs = env.reset()
+                    i = 0
+                    while (not done and not (i == time_horizon)):
+                        env.render()
+                        action, _ = policy.get_action(obs)
+                        next_obs, reward, done, info = env.step(action)
+                        trajec_per_task[completed_trajec][i] = (action, obs,
+                                                                done, reward,
+                                                                next_obs)
+                        obs = next_obs
+                        i += 1
+                    completed_trajec += 1
+            all_tasks.update({"task_" + task_id: trajec_per_task})
+            env.close()
+    np.save("garage/tf/behavioral_cloning/point_data_sequential.npy",
+            all_tasks)
+
+
+def main(mode):
     tasks = {"1": (0, 3), "2": (3, 0), "3": (0, -3), "4": (-3, 0)}
-    sample_from_experts(tasks)
+    if (mode == "-r"):
+        sample_from_experts_random(tasks)
+    elif (mode == "-s"):
+        sample_from_experts_sequential(tasks)
+    else:
+        print("The option %s is invalid." % mode)
+        print_help()
+
+
+def print_help():
+    print("Please specify one of these options:")
+    print("    -r: sample randomly")
+    print("    -s: sample sequentially")
 
 
 if __name__ == '__main__':
-    main()
+    if (len(sys.argv) > 1):
+        main(sys.argv[1])
+    else:
+        print_help()
