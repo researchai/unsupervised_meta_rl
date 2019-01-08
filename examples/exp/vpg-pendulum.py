2eac384e54ac274681b7f2e7947bc7905b59267d
Keren Zhu <naeioi@hotmail.com>
2019-01-08 11:17:54 -0800

    Update vpg-pendulum.py

--- examples/exp/vpg-pendulum.py
+++ examples/exp/vpg-pendulum.py
@@ -16,12 +16,12 @@ sampler = BatchSampler(env=env, max_path_length=100)
 
 policy = GaussianMLPPolicy(env_spec=env_spec, hidden_sizes=(32, 32))
 
-# baseline = LinearFeatureBaseline(env_spec=env_spec)
+baseline = LinearFeatureBaseline(env_spec=env_spec)
 
 agent = VPG(
     env_spec=env_spec,
     policy=policy,
-    # baseline=baseline,
+    baseline=baseline,
     discount=0.99)
 
 # Alternatives: HDFS, S3, etc.
@@ -31,6 +31,8 @@ checkpointer = DiskCheckpointer(exp_dir='garage-exp-pendulum', resume=True, pref
 logger = BasicLogger()
 """
 Initialize or load checkpoint from exp_dir.
+
+If get interrupted and restarted, experiment will resume from last checkpoint.
 
 /exp_dir
     prefix_timestamp_agent.pkl
