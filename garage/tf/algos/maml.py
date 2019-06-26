Add kl logging info and fix maml and CGoptimizer

garage/tf/algos/maml.py
@@ -26,7 +26,7 @@ class MAML(BatchPolopt):
     def __init__(self,
                  env_spec,
                  pg_loss=PGLoss.VANILLA,
-                 clip_range=0.01,
+                 clip_range=0.02,
                  optimizer=None,
                  optimizer_args=dict(),
                  name="MAML",
@@ -73,11 +73,10 @@ class MAML(BatchPolopt):
 
         # Initailize the MAML policy
         obs_flat_vars = [i.flat.obs_var for i in final_loss_inputs]
-        adaptation_inputs = [i.obs_var for i in opt_inputs]
         maml_infos, adapt_opt, adapt_opt_input = self.policy.initialize(
             gradient_vars,
             inputs=obs_flat_vars,
-            adaptation_inputs=adaptation_inputs)
+            adaptation_inputs=opt_inputs)
 
         self.f_adapt = tensor_utils.compile_function(
             flatten_inputs(opt_inputs[0]),
@@ -93,6 +92,12 @@ class MAML(BatchPolopt):
             leq_constraint=(pol_kl, self.clip_range),
             inputs=flatten_inputs([self._final_opt_inputs, self._policy_opt_inputs, self._kl_opt_inputs]),
             constraint_name="mean_kl")
+
+        self._f_adv = tensor_utils.compile_function(
+            flatten_inputs([self._final_opt_inputs, self._policy_opt_inputs, self._kl_opt_inputs]),
+            self._adv_valid,
+            log_name="adv__",
+        )
         print('Done with all initialization.')
 
     def build_inputs(self):
@@ -306,6 +311,13 @@ class MAML(BatchPolopt):
                     adv_valid = filter_valids(
                         adv_flat, i.flat.valid_var, name="adv_valid_{}".format(idx))
 
+                    eps = tf.constant(1e-8, dtype=tf.float32)
+                    # if self.center_adv:
+                    with tf.name_scope("center_adv"):
+                        mean, var = tf.nn.moments(adv_valid, axes=[0])
+                        adv_valid = tf.nn.batch_normalization(
+                            adv_valid, mean, var, 0, 1, eps)
+
                     policy_dist_info_flat = self.policy.wrapped_policy.dist_info_sym(
                         i.flat.obs_var,
                         i.flat.policy_state_info_vars,
@@ -381,6 +393,12 @@ class MAML(BatchPolopt):
                     adv_flat = flatten_batch(advantages, name="adv_flat")
                     adv_valid = filter_valids(
                         adv_flat, i.flat.valid_var, name="adv_valid")
+                    eps = tf.constant(1e-8, dtype=tf.float32)
+                    # if self.center_adv:
+                    with tf.name_scope("center_adv"):
+                        mean, var = tf.nn.moments(adv_valid, axes=[0])
+                        adv_valid = tf.nn.batch_normalization(
+                            adv_valid, mean, var, 0, 1, eps)
                     policy_dist_info_flat = dist_infos_flat[idx]
                     policy_dist_info_valid = filter_valids_dict(
                         policy_dist_info_flat,
@@ -395,9 +413,11 @@ class MAML(BatchPolopt):
                         name="lr")
 
                     surr_vanilla = lr * adv_valid
-                    losses.append(-tf.reduce_mean(surr_vanilla))
 
+                    losses.append(-tf.reduce_mean(surr_vanilla))
         surr_loss_mean = tf.reduce_mean(losses)
+
+        self._adv_valid = adv_valid
 
         with tf.name_scope("kl"):
             policy_dist_info_flat = self.policy.wrapped_policy.dist_info_sym(
@@ -493,7 +513,6 @@ class MAML(BatchPolopt):
     
     def train_once(self, itr, samples_data, adaptation_data):
         policy_opt_input_values = self.policy_opt_input_values(samples_data, adaptation_data)
-
         # Train policy network
         # logger.log("Computing loss before")
         loss_before = self.optimizer.loss(policy_opt_input_values)

garage/tf/optimizers/conjugate_gradient_optimizer.py
@@ -354,6 +354,7 @@ class ConjugateGradientOptimizer(Serializable):
                 loss, constraint_val = sliced_fun(
                     self._opt_fun["f_loss_constraint"],
                     self._num_slices)(inputs, extra_inputs)
+                print('kl:', constraint_val)
                 if self._debug_nan and np.isnan(constraint_val):
                     break
                 if loss < loss_before and \
@@ -374,7 +375,7 @@ class ConjugateGradientOptimizer(Serializable):
                 if constraint_val >= self._max_constraint_val:
                     logger.log("Violated because constraint %s is violated" %
                                self._constraint_name)
-                self._target.update_params(unflatten_tensors(cur_param, shapes))
+                self._target.update_params(unflatten_tensors(prev_param, shapes))
             logger.log("backtrack iters: %d" % n_iter)
             logger.log("computing loss after")
             logger.log("optimization finished")

garage/tf/policies/maml_policy.py
@@ -39,7 +39,7 @@ class MamlPolicy(StochasticPolicy2):
         self._create_update_opts()
 
 
-    def initialize(self, gradient_var, inputs, adaptation_inputs):
+    def initialize(self, gradient_var, inputs, adaptation_inputs, stop_second_order=True):
         """
         Initialize the MAML Policy.
 
@@ -71,23 +71,29 @@ class MamlPolicy(StochasticPolicy2):
 
                     for p, g in zip(params, gradient_i):
                         adapted_param = p - self._adaptation_step_size * g
+                        if stop_second_order:
+                            adapted_param = tf.stop_gradient(adapted_param)
                         name = "maml_policy/{}/{}".format(i, p.name)
                         self._adapted_param_store[name] = adapted_param
                         if i == 0:
                             update_opts.append(adapted_param)
 
-            def maml_get_variable(name, shape=None, **kwargs):
+            def maml_get_variable(name, shape=None, last=False, **kwargs):
                 scope = tf.get_variable_scope()
-                idx = 0
+                # idx = 0
+                # TODO add idx back........
                 if MAML_TASK_INDEX >= self.meta_batch_size:
                     raise ValueError("Invalid TASK_INDEX.")
+                # fullname = "maml_policy/{}/{}/{}:{}".format(
+                #     MAML_TASK_INDEX, scope.name, name, idx)
+                # while fullname in MAML_VARIABLE_STORE:
+                #     idx += 1
+                #     fullname = "maml_policy/{}/{}/{}:{}".format(
+                #         MAML_TASK_INDEX, scope.name, name, idx)
                 fullname = "maml_policy/{}/{}/{}:{}".format(
-                    MAML_TASK_INDEX, scope.name, name, idx)
-                while fullname in MAML_VARIABLE_STORE:
-                    idx += 1
-                    fullname = "maml_policy/{}/{}/{}:{}".format(
-                        MAML_TASK_INDEX, scope.name, name, idx)
+                        MAML_TASK_INDEX, scope.name, name, 0)
                 MAML_VARIABLE_STORE.add(fullname)
+                # print(fullname)
                 return self._adapted_param_store[fullname]
 
             # build the model with these parameters
@@ -108,8 +114,6 @@ class MamlPolicy(StochasticPolicy2):
 
             self._initialized = True
 
-        # Use the original get_variable
-        variable_scope.get_variable = original_get_variable
         update_opts_input = inputs[0]
 
         # Build get_action_with_adaptation_data.
@@ -129,6 +133,9 @@ class MamlPolicy(StochasticPolicy2):
             tensor_utils.flatten_inputs([input_ph, adaptation_inputs[-1]]),
             outputs,
             log_name='get_action_with_adaptation_data',)
+
+        # Use the original get_variable
+        variable_scope.get_variable = original_get_variable
 
         return self._all_model_outputs, update_opts, update_opts_input
 