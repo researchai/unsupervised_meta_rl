9507b04219db2b969992768a538cc373b06d5a34
CatherineSue <8605658+CatherineSue@users.noreply.github.com>
2019-07-02 22:58:25 -0700

    Increase pol_ent_coeff

--- corl/mtppo_easy_mode_10tasks.py
+++ corl/mtppo_easy_mode_10tasks.py
@@ -88,7 +88,7 @@ def run_task(*_):
             baseline=baseline,
             max_path_length=150,
             discount=0.99,
-            gae_lambda=0.95,
+            gae_lambda=1,
             lr_clip_range=0.2,
             optimizer_args=dict(
                 batch_size=32,
@@ -96,7 +96,7 @@ def run_task(*_):
             ),
             stop_entropy_gradient=False,
             entropy_method='regularized',
-            policy_ent_coeff=1e-3,
+            policy_ent_coeff=5e-3,
             center_adv=True,
         )
 
