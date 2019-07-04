1920f8d18006084b2601c008dc7c8534a3c2d4d8
CatherineSue <8605658+CatherineSue@users.noreply.github.com>
2019-07-04 16:10:03 -0700

    Derease lr_clip_range to 0.3

--- corl/mtppo_easy_mode_10tasks.py
+++ corl/mtppo_easy_mode_10tasks.py
@@ -89,7 +89,7 @@ def run_task(*_):
             max_path_length=150,
             discount=0.99,
             gae_lambda=1,
-            lr_clip_range=0.4,
+            lr_clip_range=0.3,
             optimizer_args=dict(
                 batch_size=32,
                 max_epochs=10,
