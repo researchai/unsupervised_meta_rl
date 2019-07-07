c2753a366cc4d9424aca1974711645e01903c651
zhanpenghe <zhanpenh@usc.edu>
2019-07-06 17:53:09 -0700

    Remove wrong argument in mtppo.sh

--- corl/mtppo.sh
+++ corl/mtppo.sh
@@ -2,5 +2,5 @@ echo "$1"
 
 for i in $(seq $2 $3)
 do
-    PYTHONPATH=/root/code/garage:/root/code/garage/metaworld:/root/code/garage/corl/baby python ./mtppo_easy_mode_test.py 0 --snapshot_dir $1 --itr $i
+    PYTHONPATH=/root/code/garage:/root/code/garage/metaworld:/root/code/garage/corl/baby python ./mtppo_easy_mode_test.py --snapshot_dir $1 --itr $i
 done
