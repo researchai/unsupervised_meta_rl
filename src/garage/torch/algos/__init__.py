78b5beaf0eec89592c7299f86c67d5c1624da491
Linda Wong <y.wong92@gmail.com>
2019-11-20 15:25:43 -0800

    Add new modules for PEARL
    - RecurrentEncoder
    - PEARLInferenceNetwork

--- src/garage/torch/algos/__init__.py
+++ src/garage/torch/algos/__init__.py
@@ -3,5 +3,8 @@ from garage.torch.algos.ddpg import DDPG
 from garage.torch.algos.vpg import VPG
 from garage.torch.algos.ppo import PPO  # noqa: I100
 from garage.torch.algos.trpo import TRPO
+from garage.torch.algos.pearl_inference_network import PEARLInferenceNetwork
+from garage.torch.algos.recurrent_encoder import RecurrentEncoder
 
-__all__ = ['DDPG', 'VPG', 'PPO', 'TRPO']
+__all__ = ['DDPG', 'VPG', 'PPO', 'TRPO', 'PEARLInferenceNetwork', 
+    'RecurrentEncoder']
