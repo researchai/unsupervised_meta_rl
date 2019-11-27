8c827d7aed95647f07e9937ef79b6f9f02afc800
Linda Wong <y.wong92@gmail.com>
2019-11-27 13:40:37 -0800

    Address comments

--- src/garage/torch/algos/__init__.py
+++ src/garage/torch/algos/__init__.py
@@ -1,11 +1,7 @@
 """PyTorch algorithms."""
 from garage.torch.algos.ddpg import DDPG
-from garage.torch.algos.pearl_inference_network import PEARLInferenceNetwork
-from garage.torch.algos.ppo import PPO  # noqa: I100
-from garage.torch.algos.recurrent_encoder import RecurrentEncoder
-from garage.torch.algos.trpo import TRPO
 from garage.torch.algos.vpg import VPG
+from garage.torch.algos.ppo import PPO  # noqa: I100
+from garage.torch.algos.trpo import TRPO
 
-__all__ = [
-    'DDPG', 'VPG', 'PPO', 'TRPO', 'PEARLInferenceNetwork', 'RecurrentEncoder'
-]
+__all__ = ['DDPG', 'VPG', 'PPO', 'TRPO']
--- src/garage/torch/policies/__init__.py
+++ src/garage/torch/policies/__init__.py
@@ -1,7 +1,12 @@
 """PyTorch Policies."""
 from garage.torch.policies.base import Policy
+from garage.torch.policies.context_conditioned_policy import (
+    ContextConditionedPolicy)
 from garage.torch.policies.deterministic_mlp_policy import (
     DeterministicMLPPolicy)
 from garage.torch.policies.gaussian_mlp_policy import GaussianMLPPolicy
 
-__all__ = ['DeterministicMLPPolicy', 'GaussianMLPPolicy', 'Policy']
+__all__ = [
+    'ContextConditionedPolicy', 'DeterministicMLPPolicy', 'GaussianMLPPolicy',
+    'Policy'
+]
