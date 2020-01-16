b397360eafdf30cec30729e0f3737fff103abd69
tsan <ahtsans@gmail.com>
2020-01-15 20:58:49 -0800

    New sampler API

--- examples/tf/rl2_halfcheetah.py
+++ examples/tf/rl2_halfcheetah.py
@@ -11,7 +11,7 @@ from garage.tf.optimizers import FiniteDifferenceHvp
 from garage.tf.policies import GaussianGRUPolicy
 from garage.sampler.rl2_sampler import RL2Sampler
 
-from metaworld.benchmarks import ML1
+# from metaworld.benchmarks import ML1
 import os
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
@@ -27,23 +27,26 @@ def run_task(snapshot_config, *_):
 
     """
     with LocalTFRunner(snapshot_config=snapshot_config) as runner:
-        # env = RL2Env(env=HalfCheetahVelEnv())
-        # env2 = RL2Env(env=HalfCheetahRandVelEnv())
-        # env = RL2Env(env=HalfCheetahRandDirecEnv())
-        env = RL2Env(ML1.get_train_tasks('push-v1'))
-        policy = GaussianGRUPolicy(name='policy',
-                                   hidden_dim=64,
-                                   env_spec=env.spec,
-                                   state_include_action=False)
-
-        baseline = LinearFeatureBaseline(env_spec=env.spec)
-
         max_path_length = 100
         meta_batch_size = 200
-        n_epochs = 100
-        episode_per_task = 40
+        n_epochs = 500
+        episode_per_task = 10
 
-        inner_algo = PPO(env_spec=env.spec,
+        envs = [RL2Env(env=HalfCheetahVelEnv()) for _ in range(meta_batch_size)]
+        tasks = envs[0].sample_tasks(meta_batch_size)
+        for i, task in enumerate(tasks):
+            envs[i].set_task(task)
+        # env2 = RL2Env(env=HalfCheetahRandVelEnv())
+        # env = RL2Env(env=HalfCheetahRandDirecEnv())
+        # env = RL2Env(ML1.get_train_tasks('push-v1'))
+        policy = GaussianGRUPolicy(name='policy',
+                                   hidden_dim=64,
+                                   env_spec=envs[0].spec,
+                                   state_include_action=False)
+
+        baseline = LinearFeatureBaseline(env_spec=envs[0].spec)
+
+        inner_algo = PPO(env_spec=envs[0].spec,
                          policy=policy,
                          baseline=baseline,
                          max_path_length=max_path_length * episode_per_task,
@@ -62,10 +65,9 @@ def run_task(snapshot_config, *_):
                    positive_adv=False)
 
         runner.setup(algo,
-                     env,
+                     envs,
                      sampler_cls=RL2Sampler,
                      sampler_args=dict(meta_batch_size=meta_batch_size,
-                                       episode_per_task=episode_per_task,
                                        n_envs=meta_batch_size))
         runner.train(n_epochs=n_epochs,
                      batch_size=episode_per_task * max_path_length *
