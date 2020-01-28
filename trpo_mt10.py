a3781dc2e56b4c8067d2fadba09ce611389b3af9
yonghyuc <yonghyuc@usc.edu>
2020-01-27 18:52:10 -0800

    add trpo mt 10

--- trpo_mt10.py
+++ trpo_mt10.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-"""
-This is an example to train a task with PPO algorithm.
+"""An example to train a task with TRPO algorithm."""
+import gym
 
-Here it creates InvertedDoublePendulum using gym. And uses a PPO with 1M
-steps.
-
-Results:
-    AverageDiscountedReturn: 500
-    RiseTime: itr 40
-
-"""
+from garage.experiment import run_experiment
+from garage.np.baselines import LinearFeatureBaseline
+from garage.tf.algos import TRPO
+from garage.tf.envs import TfEnv
+from garage.tf.experiment import LocalTFRunner
+from garage.tf.policies import GaussianMLPPolicy
 import random
-
 import tensorflow as tf
 from garage.envs import normalize
 from garage.envs.multi_env_wrapper import MultiEnvWrapper, round_robin_strategy
@@ -31,61 +28,44 @@ MT10_envs_by_id = {
     for (task, env) in EASY_MODE_CLS_DICT.items()
 }
 
-# env_ids = ['reach-v1', 'push-v1', 'pick-place-v1', 'door-v1', 'drawer-open-v1', 'drawer-close-v1', 'button-press-topdown-v1', 'ped-insert-side-v1', 'window-open-v1', 'window-close-v1']
+env_ids = ['reach-v1', 'push-v1', 'pick-place-v1', 'door-v1', 'drawer-open-v1', 'drawer-close-v1', 'button-press-topdown-v1', 'ped-insert-side-v1', 'window-open-v1', 'window-close-v1']
 # env_ids = ['push-v1']
 # env_ids = ['reach-v1']
-env_ids = ['pick-place-v1']
+# env_ids = ['pick-place-v1']
 
 MT10_envs = [TfEnv(normalize(MT10_envs_by_id[i])) for i in env_ids]
 
+
 @wrap_experiment
-def ppo_mt10(ctxt=None, seed=1):
+def trpo_mt10(ctxt=None, seed=1):
 
     """Run task."""
     set_seed(seed)
     with LocalTFRunner(snapshot_config=ctxt) as runner:
         env = MultiEnvWrapper(MT10_envs, env_ids, sample_strategy=round_robin_strategy)
 
-        policy = GaussianMLPPolicy(
-            env_spec=env.spec,
-            hidden_sizes=(64, 64),
-            hidden_nonlinearity=tf.nn.tanh,
-            output_nonlinearity=None,
-        )
+        policy = GaussianMLPPolicy(env_spec=env.spec, hidden_sizes=(64, 64))
 
+        # baseline = LinearFeatureBaseline(env_spec=env.spec)
         baseline = GaussianMLPBaseline(
             env_spec=env.spec,
             regressor_args=dict(
                 hidden_sizes=(64, 64),
-                use_trust_region=True,
+                use_trust_region=False,
             ),
         )
 
-        algo = PPO(
-            env_spec=env.spec,
-            policy=policy,
-            baseline=baseline,
-            max_path_length=100,
-            discount=0.99,
-            gae_lambda=0.95,
-            lr_clip_range=0.2,
-            optimizer_args=dict(
-                batch_size=32,
-                max_epochs=10,
-                tf_optimizer_args=dict(
-                    learning_rate=1e-3,
-                ),
-            ),
-            stop_entropy_gradient=True,
-            entropy_method='max',
-            policy_ent_coeff=0.002,
-            center_adv=False,
-        )
+        algo = TRPO(env_spec=env.spec,
+                    policy=policy,
+                    baseline=baseline,
+                    max_path_length=500,
+                    discount=0.99,
+                    max_kl_step=0.01)
 
         runner.setup(algo, env)
-        runner.train(n_epochs=250, batch_size=len(), plot=False)
+        runner.train(n_epochs=40, batch_size=len(MT10_envs)*10*150)
 
 
 seeds = random.sample(range(100), 3)
 for seed in seeds:
-    ppo_mt10(seed=seed)
+    trpo_mt10(seed=seed)
