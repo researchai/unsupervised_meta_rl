16dffb56c6fda0f052cf76851c1d4e11b1598eb6
tsan <ahtsans@gmail.com>
2020-01-28 00:28:09 -0800

    handle different obs space and ML45 ready

--- src/garage/envs/rl2_env.py
+++ src/garage/envs/rl2_env.py
@@ -29,8 +29,9 @@ class RL2Env(gym.Wrapper):
 
     """
 
-    def __init__(self, env):
+    def __init__(self, env, max_obs_dim=None):
         super().__init__(env)
+        self.max_obs_dim = max_obs_dim
         action_space = akro.from_gym(self.env.action_space)
         observation_space = akro.from_gym(self._create_rl2_obs_space(env))
         self.spec = EnvSpec(action_space=action_space,
@@ -39,16 +40,23 @@ class RL2Env(gym.Wrapper):
     def _create_rl2_obs_space(self, env):
         obs_flat_dim = np.prod(env.observation_space.shape)
         action_flat_dim = np.prod(env.action_space.shape)
+        if self.max_obs_dim is not None:
+            obs_flat_dim = self.max_obs_dim
         return gym.spaces.Box(low=-np.inf,
                               high=np.inf,
                               shape=(obs_flat_dim + action_flat_dim + 1 + 1,))
 
     def reset(self):
         obs = self.env.reset()
+        # pad zeros if needed for running ML45
+        if self.max_obs_dim is not None:
+            obs = np.concatenate([obs, np.zeros(self.max_obs_dim - obs.shape[0])])
         return np.concatenate([obs, np.zeros(self.env.action_space.shape), [0], [0]])
 
     def step(self, action):
         next_obs, reward, done, info = self.env.step(action)
+        if self.max_obs_dim is not None:
+            next_obs = np.concatenate([next_obs, np.zeros(self.max_obs_dim - next_obs.shape[0])])
         next_obs = np.concatenate([next_obs, action, [reward], [done]])
         return next_obs, reward, done, info
 
