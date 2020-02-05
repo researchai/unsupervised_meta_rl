cb1c23750c2ec25f30c46f46d60fee4480f998fc
yonghyuc <yonghyuc@usc.edu>
2020-02-05 00:30:17 -0800

    chagne ppo_mt10 for max entropy

--- ppo_mt10.py
+++ ppo_mt10.py
@@ -76,6 +76,10 @@ def ppo_mt10(ctxt=None, seed=1):
                     learning_rate=3e-4,
                 ),
             ),
+            stop_entropy_gradient=True,
+            entropy_method='max',
+            policy_ent_coeff=0.1,
+            center_adv=False,
         )
 
         runner.setup(algo, env)
