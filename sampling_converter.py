f7b75d90f4258e6577bab20ee37569792e8db10a
yonghyuc <yonghyuc@usc.edu>
2020-02-05 18:31:04 -0800

    add sampling log converter

---
+++ sampling_converter.py
@@ -0,0 +1,38 @@
+import pandas as pd
+divider = '--------------------------'
+
+
+def _convert_log_to_dicts(folder_name):
+    data = []
+    line = "??"
+    with open(folder_name+'/debug.log') as f:
+        while line != "":
+            while divider not in line:
+                line = f.readline()
+            line = f.readline()
+            #         print (line.split())
+            epoch_data = dict()
+            while divider not in line:
+                attribute = line.split()
+                epoch_data[attribute[0]] = attribute[1]
+                line = f.readline()
+            line = f.readline()
+
+            data.append(epoch_data)
+    return data
+
+
+def _extract_columns(data):
+    columns = set()
+    for d in data:
+        columns |= set(d.keys())
+    return columns
+
+
+def convert(folder_name):
+    data = _convert_log_to_dicts(folder_name)
+    columns = _extract_columns(data)
+    df = pd.DataFrame(columns=columns)
+    for d in data:
+        df = df.append(d, ignore_index=True)
+    df.to_csv(folder_name+'/converted_progress.csv', index=False)
