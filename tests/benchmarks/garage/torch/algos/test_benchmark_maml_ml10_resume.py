768fc95007894bbcf0fb92f9cc486b3138974da5
Keren Zhu <naeioi@hotmail.com>
2020-02-06 03:55:50 -0800

    Add ml10 resuming

---
+++ tests/benchmarks/garage/torch/algos/test_benchmark_maml_ml10_resume.py
@@ -0,0 +1,44 @@
+from functools import partial
+import os
+import sys
+
+from dowel import logger, StdOutput, CsvOutput, tabular, TensorBoardOutput
+
+from metaworld.benchmarks import ML10WithPinnedGoal
+
+from garage.envs.TaskIdWrapper import TaskIdWrapper
+from garage.experiment import MetaEvaluator, Snapshotter
+from garage.experiment import LocalRunner, SnapshotConfig
+from garage.experiment.task_sampler import AllSetTaskSampler
+
+
+meta_train_dirs = []
+max_path_length = 150
+adapt_rollout_per_task = 10
+test_rollout_per_task = 10
+
+meta_task_cls = ML10WithPinnedGoal.get_test_tasks
+
+
+def resume_training(meta_train_dir):
+    logger.add_output(CsvOutput(os.path.join(meta_train_dir, 'progress-resume.csv')))
+    logger.add_output(TensorBoardOutput(meta_train_dir))
+    logger.add_output(StdOutput())
+
+    snapshot_config = SnapshotConfig(snapshot_dir=meta_train_dir,
+                                     snapshot_mode='all',
+                                     snapshot_gap=1)
+
+    runner = LocalRunner(snapshot_config=snapshot_config)
+    runner.restore(meta_train_dir)
+    runner.resume()
+
+    logger.remove_output_type(CsvOutput)
+    logger.remove_output_type(StdOutput)
+    logger.remove_output_type(TensorBoardOutput)
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    meta_train_dir = sys.argv[1]
+
+    resume_training(meta_train_dir)
