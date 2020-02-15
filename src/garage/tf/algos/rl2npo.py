4b8f53202ac0971c1782ccaff517fd7b0a7ee0ab
tsan <ahtsans@gmail.com>
2020-02-15 09:47:09 -0800

    Comment some code to run faster

--- src/garage/tf/algos/rl2npo.py
+++ src/garage/tf/algos/rl2npo.py
@@ -17,23 +17,21 @@ class RL2NPO(NPO):
                 See process_samples() for details.
 
         """
-        policy_opt_input_values = self._policy_opt_input_values(samples_data)
+        # policy_opt_input_values = self._policy_opt_input_values(samples_data)
 
-        # Augment reward from baselines
-        rewards_tensor = self._f_rewards(*policy_opt_input_values)
-        returns_tensor = self._f_returns(*policy_opt_input_values)
-        returns_tensor = np.squeeze(returns_tensor, -1)
-        adv = self._f_adv(*policy_opt_input_values)
+        # # Augment reward from baselines
+        # rewards_tensor = self._f_rewards(*policy_opt_input_values)
+        # returns_tensor = self._f_returns(*policy_opt_input_values)
+        # returns_tensor = np.squeeze(returns_tensor, -1)
         
         paths = samples_data['paths']
-        valids = samples_data['valids']
+        # valids = samples_data['valids']
 
-        # baselines = []
         # Fit baseline
         logger.log('Fitting baseline...')
-        for ind, path in enumerate(paths):
-            path['rewards'] = rewards_tensor[ind][valids[ind].astype(np.bool)]
-            path['returns'] = returns_tensor[ind][valids[ind].astype(np.bool)]
+        # for ind, path in enumerate(paths):
+        #     path['rewards'] = rewards_tensor[ind][valids[ind].astype(np.bool)]
+        #     path['returns'] = returns_tensor[ind][valids[ind].astype(np.bool)]
 
         self.baseline.fit(paths)
         baselines = [self.baseline.predict(path) for path in paths]
