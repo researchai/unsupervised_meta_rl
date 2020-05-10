Fix bug in pixel normalization

examples/tf/ddpg_pendulum_pixels.py
@@ -11,7 +11,7 @@ from garage.envs.wrappers import Grayscale
 from garage.envs.wrappers import MaxAndSkip
 from garage.envs.wrappers import PixelObservation
 from garage.envs.wrappers import Resize
-from garage.envs.wrappers import StackFrames
+from garage.envs.normalized_env import normalize
 from garage.experiment import run_experiment
 from garage.experiment.deterministic import set_seed
 from garage.np.exploration_strategies import OUStrategy
@@ -34,30 +34,37 @@ def ddpg_pendulum_pixels(ctxt=None, seed=1, buffer_size=int(1e4)):
     with LocalTFRunner(snapshot_config=ctxt, max_cpus=4) as runner:
         
         env = gym.make('Pendulum-v0')
+        env = normalize(env) # scale action space to -1, 1
         env.reset() # fixes bug when using gym.wrappers.PixelObservation and Pendulum
         env = PixelObservation(env)
         env = Grayscale(env)
         env = Resize(env, 86, 86)
-        env = StackFrames(env, 2)
-        
+        #env = StackFrames(env, 2)
         env = TfEnv(env, is_image=True)
+        
 
         action_noise = OUStrategy(env.spec, sigma=0.2)
 
+        #tf.debugging.enable_check_numerics()
+
         policy = ContinuousCNNPolicy(env_spec=env.spec,
                                      filter_dims=(8, 4, 3),
-                                     num_filters=(32, 64, 64),
+                                     num_filters=(32, 32, 32),
                                      strides=(4, 2, 1),
-                                     hidden_sizes=(64, 64),
+                                     hidden_sizes=(64, 64), #(200,200) in ddpg paper
                                      hidden_nonlinearity=tf.nn.relu,
+                                     output_b_init=tf.random_uniform_initializer(-3e-4, 3e-4),
+                                     output_w_init=tf.random_uniform_initializer(-3e-4, 3e-4),
                                      output_nonlinearity=tf.nn.tanh)
         qf = ContinuousCNNQFunction(env_spec=env.spec,
                                     filter_dims=(8, 4, 3),
-                                    num_filters=(32, 64, 64),
+                                    num_filters=(32, 32, 32),
                                     strides=(4, 2, 1),
-                                    hidden_sizes=[64, 64],
+                                    hidden_sizes=[64, 64], #(200,200) in ddpg paper
                                     hidden_nonlinearity=tf.nn.relu,
-                                    action_merge_layer=0)
+                                    output_b_init=tf.random_uniform_initializer(-3e-4, 3e-4),
+                                    output_w_init=tf.random_uniform_initializer(-3e-4, 3e-4),
+                                    action_merge_layer=1)
 
         replay_buffer = SimpleReplayBuffer(
             env_spec=env.spec,
@@ -66,14 +73,16 @@ def ddpg_pendulum_pixels(ctxt=None, seed=1, buffer_size=int(1e4)):
 
         ddpg = DDPG(env_spec=env.spec,
                     policy=policy,
-                    policy_lr=1e-4,
+                    policy_lr=1e-6,
                     qf_lr=1e-3,
                     qf=qf,
+                    qf_weight_decay=1e-2,
                     replay_buffer=replay_buffer,
-                    max_path_length=100,
+                    buffer_batch_size=16,
                     steps_per_epoch=20,
                     target_update_tau=1e-2,
                     n_train_steps=50,
+                    max_path_length=100,
                     discount=0.99,
                     min_buffer_size=int(1e4),
                     exploration_strategy=action_noise,

src/garage/envs/wrappers/resize.py
@@ -44,7 +44,7 @@ class Resize(gym.Wrapper):
         self._dtype = env.observation_space.dtype
         self._observation_space = gym.spaces.Box(_low,
                                                  _high,
-                                                 shape=[width, height],
+                                                 shape=[width, height, 1],
                                                  dtype=self._dtype)
 
         self._width = width
@@ -67,7 +67,7 @@ class Resize(gym.Wrapper):
             2. anti-aliasing will be enabled by default in skimage 0.15
             """
             warnings.simplefilter('ignore')
-            obs = resize(obs, (self._width, self._height))  # now it's float
+            obs = resize(obs, (self._width, self._height, 1))  # now it's float
             if self._dtype == np.uint8:
                 obs = img_as_ubyte(obs)
         return obs

src/garage/tf/algos/ddpg.py
@@ -1,6 +1,7 @@
 """Deep Deterministic Policy Gradient (DDPG) implementation in TensorFlow."""
 from collections import deque
 
+import akro
 from dowel import logger, tabular
 import numpy as np
 import tensorflow as tf
@@ -105,6 +106,7 @@ class DDPG(OffPolicyRLAlgorithm):
         self.episode_qf_losses = []
         self.epoch_ys = []
         self.epoch_qs = []
+        self.q_func_grad_norms = []
 
         self.target_policy = policy.clone('target_policy')
         self.target_qf = qf.clone('target_qf')
@@ -130,10 +132,10 @@ class DDPG(OffPolicyRLAlgorithm):
         with tf.name_scope(self.name):
             # Create target policy and qf network
             self.target_policy_f_prob_online = tensor_utils.compile_function(
-                inputs=[self.target_policy.model.networks['default'].input],
+                inputs=[self.target_policy.input],
                 outputs=self.target_policy.model.networks['default'].outputs)
             self.target_qf_f_prob_online = tensor_utils.compile_function(
-                inputs=self.target_qf.model.networks['default'].inputs,
+                inputs=self.target_qf.inputs,
                 outputs=self.target_qf.model.networks['default'].outputs)
 
             # Set up target init and update function
@@ -156,15 +158,31 @@ class DDPG(OffPolicyRLAlgorithm):
             with tf.name_scope('inputs'):
                 if not self.flatten_obses:
                     obs_dim = self.env_spec.observation_space.shape
-                    obs = tf.compat.v1.placeholder(tf.float32,
+
+                    if isinstance(self.env_spec.observation_space, akro.Image):
+                        obs = tf.compat.v1.placeholder(tf.uint8,
+                                               shape=(None, ) + obs_dim,
+                                               name='input_observation')
+                    else:
+                        obs = tf.compat.v1.placeholder(tf.float32,
                                                shape=(None, ) + obs_dim,
                                                name='input_observation')
 
                 else:
                     obs_dim = self.env_spec.observation_space.flat_dim
-                    obs = tf.compat.v1.placeholder(tf.float32,
+
+                    if isinstance(self.env_spec.observation_space, akro.Image):
+                        obs = tf.compat.v1.placeholder(tf.uint8,
                                                shape=(None, obs_dim),
                                                name='input_observation')
+                    else:
+                        obs = tf.compat.v1.placeholder(tf.float32,
+                                               shape=(None, obs_dim),
+                                               name='input_observation')
+
+                    # obs = tf.compat.v1.placeholder(tf.float32,
+                    #                            shape=(None, obs_dim),
+                    #                            name='input_observation')
                 input_y = tf.compat.v1.placeholder(tf.float32,
                                                    shape=(None, 1),
                                                    name='input_y')
@@ -186,14 +204,22 @@ class DDPG(OffPolicyRLAlgorithm):
                     for var in self.policy.get_regularizable_vars():
                         policy_reg = regularizer(var)
                         action_loss += policy_reg
-
+            
+            
             with tf.name_scope('minimize_action_loss'):
-                policy_train_op = self.policy_optimizer(
-                    self.policy_lr, name='PolicyOptimizer').minimize(
-                        action_loss, var_list=self.policy.get_trainable_vars())
+                # policy_train_op = self.policy_optimizer(
+                #     self.policy_lr, name='PolicyOptimizer').minimize(
+                #         action_loss, var_list=self.policy.get_trainable_vars())
+                adam = self.policy_optimizer(self.policy_lr, name='PolicyOptimizer')
+                grads = adam.compute_gradients(action_loss,var_list=self.policy.get_trainable_vars())
+                # print(grads)
 
+                # for i, grad in enumerate(grads):
+                #      tf.summary.histogram("{}-grad".format(grads[i][1].name), grads[i])
+
+                policy_train_op = adam.apply_gradients(grads)
             f_train_policy = tensor_utils.compile_function(
-                inputs=[obs], outputs=[policy_train_op, action_loss])
+                inputs=[obs], outputs=[policy_train_op, action_loss, grads])
 
             # Set up qf training function
             qval = self.qf.get_qval_sym(obs, actions, name='q_value')
@@ -208,13 +234,19 @@ class DDPG(OffPolicyRLAlgorithm):
                         qval_loss += qf_reg
 
             with tf.name_scope('minimize_qf_loss'):
-                qf_train_op = self.qf_optimizer(
-                    self.qf_lr, name='QFunctionOptimizer').minimize(
-                        qval_loss, var_list=self.qf.get_trainable_vars())
+                adam_qf = self.qf_optimizer(self.qf_lr, name='QFunctionOptimizer')
+                #raw_grads, variables = zip(*adam_qf.compute_gradients(qval_loss, var_list=self.qf.get_trainable_vars()))
+                grads = adam_qf.compute_gradients(qval_loss, var_list=self.qf.get_trainable_vars())
+                grads = [(tf.clip_by_norm(grad, 5.0), var) for grad, var in grads]
+                qf_train_op = adam_qf.apply_gradients(grads)
+
+                # qf_train_op = self.qf_optimizer(
+                #     self.qf_lr, name='QFunctionOptimizer').minimize(
+                #         qval_loss, var_list=self.qf.get_trainable_vars())
 
             f_train_qf = tensor_utils.compile_function(
                 inputs=[input_y, obs, actions],
-                outputs=[qf_train_op, qval_loss, qval])
+                outputs=[qf_train_op, qval_loss, qval, grads])
 
             self.f_train_policy = f_train_policy
             self.f_train_qf = f_train_qf
@@ -301,6 +333,8 @@ class DDPG(OffPolicyRLAlgorithm):
                                np.mean(np.abs(self.epoch_ys)))
                 tabular.record('AverageSuccessRate',
                                np.mean(self.success_history))
+                tabular.record('QFunctionGradNorm',
+                                np.mean(self.q_func_grad_norms))
 
             if not self.smooth_return:
                 self.episode_rewards = []
@@ -308,6 +342,7 @@ class DDPG(OffPolicyRLAlgorithm):
                 self.episode_qf_losses = []
                 self.epoch_ys = []
                 self.epoch_qs = []
+                self.q_func_grad_norms = []
 
             self.success_history.clear()
         return last_average_return
@@ -326,6 +361,7 @@ class DDPG(OffPolicyRLAlgorithm):
             float: Q value predicted by the q network.
 
         """
+        
         transitions = self.replay_buffer.sample(self.buffer_batch_size)
         observations = transitions['observation']
         rewards = transitions['reward']
@@ -349,9 +385,50 @@ class DDPG(OffPolicyRLAlgorithm):
             rewards + (1.0 - terminals) * self.discount * target_qvals,
             clip_range[0], clip_range[1])
 
-        _, qval_loss, qval = self.f_train_qf(ys, inputs, actions)
-        _, action_loss = self.f_train_policy(inputs)
+        _, qval_loss, qval, grads = self.f_train_qf(ys, inputs, actions)
 
+        
+        #print("qval: " + str(qval) + " ys: " + str(ys) + " action[0]: " + str(actions[0]))
+
+        grad_norm = np.linalg.norm(grads[len(grads)-2])
+
+        norms = [np.linalg.norm(grad) for grad, _ in grads]
+        self.q_func_grad_norms.extend(norms)
+        # print("output grad norm: " + str(grad_norm))
+        # print ("all: " + str(norms))
+        #print("grads: " + str(grads[len(grads)-1]))
+        # print("vars: " +str(variables[len(variables)-1]))
+        #raise ValueError()
+        #self.q_func_grad_norms.extend()
+        # gr = tf.compat.v1.get_default_graph()
+        #kernel = gr.get_tensor_by_name('ContinuousCNNQFunction/CNNMLPMergeModel/MLPMergeModel/mlp_concat/output/kernel:0').eval()
+        # print(kernel)
+        # print(np.amax(kernel))
+        # print(np.amin(kernel))
+        # print("before: ")
+
+        # for var in self.policy.get_trainable_vars():
+        #     assert not np.isnan(np.sum(tf.compat.v1.get_default_session().run(var)))
+
+        _, action_loss, grads = self.f_train_policy(inputs)
+        #print(grads)
+        norms = [np.linalg.norm(grad) for grad, _ in grads]
+        #print ("all policy norms: " + str(norms))
+        # print("CHECKING VARS")
+        # for var in self.policy.get_trainable_vars():
+        #     if np.isnan(np.sum(tf.compat.v1.get_default_session().run(var))):
+        #         print("obs: " + str(observations))
+        #         # print("rew: " + str(rewards))
+        #         # print("term: " + str(terminals))
+        #         # print("act: " + str(actions))
+        #         # print("next obs: " + str(next_observations))
+        #         # print("targ act: " + str(target_actions))
+        #         # print("targ qvals: " + str(target_qvals))
+        #         # print("clip: " + str(clip_range))
+        #         # print("ys: " + str(ys))
+        #         raise ValueError()
+ 
+        
+        # print("OK")
         self.f_update_target()
-
         return qval_loss, ys, qval, action_loss

src/garage/tf/policies/continuous_cnn_policy.py
@@ -124,24 +124,27 @@ class ContinuousCNNPolicy(Policy):
             state_input = tf.compat.v1.placeholder(tf.uint8,
                                                    shape=(None, ) +
                                                    self.obs_dim)
-            state_input = tf.cast(state_input, tf.float32)
-            state_input /= 255.0
+            augmented_state_input = tf.cast(state_input, tf.float32)
+            augmented_state_input /= 255.0
         else:
             state_input = tf.compat.v1.placeholder(tf.float32,
                                                    shape=(None, ) +
                                                    self.obs_dim)
-
         with tf.compat.v1.variable_scope(self.name) as vs:
             self._variable_scope = vs
-            self.model.build(state_input)
-
+            self.model.build(augmented_state_input)
         self._f_prob = tf.compat.v1.get_default_session().make_callable(
-            self.model.outputs, feed_list=[self.model.input])
+            self.model.outputs, feed_list=[state_input])
 
+        self._obs_input = state_input
     @property
     def vectorized(self):
         """bool: True if primitive supports vectorized operations."""
         return True
+
+    @property
+    def input(self):
+        return self._obs_input
 
     def get_action_sym(self, obs_var, name=None):
         r"""Symbolic graph of the action.
@@ -156,9 +159,10 @@ class ContinuousCNNPolicy(Policy):
 
         """
         with tf.compat.v1.variable_scope(self._variable_scope):
+            augmented_obs_var = obs_var
             if isinstance(self._obs_space, akro.Image):
-                obs_var = tf.cast(obs_var, tf.float32) / 255.0
-            return self.model.build(obs_var, name=name)
+                augmented_obs_var = tf.cast(obs_var, tf.float32) / 255.0
+            return self.model.build(augmented_obs_var, name=name)
 
     def get_action(self, observation):
         """Get single action from this policy for the input observation.
@@ -236,6 +240,7 @@ class ContinuousCNNPolicy(Policy):
         """
         new_dict = super().__getstate__()
         del new_dict['_f_prob']
+        del new_dict['_obs_input']
         return new_dict
 
     def __setstate__(self, state):

src/garage/tf/q_functions/continuous_cnn_q_function.py
@@ -144,21 +144,27 @@ class ContinuousCNNQFunction(QFunction):
         self._initialize()
 
     def _initialize(self):
-        obs_ph = tf.compat.v1.placeholder(tf.float32, (None, ) + self._obs_dim,
-                                          name='state')
+        
         action_ph = tf.compat.v1.placeholder(tf.float32,
                                              (None, ) + self._action_dim,
                                              name='action')
         if isinstance(self._env_spec.observation_space, akro.Image):
-            obs_ph = obs_ph / 255.0
-
+            obs_ph = tf.compat.v1.placeholder(tf.uint8, (None, ) + self._obs_dim,
+                                          name='state')
+            augmented_obs_ph = tf.cast(obs_ph, tf.float32) / 255.0
+        else:
+            obs_ph = tf.compat.v1.placeholder(tf.float32, (None, ) + self._obs_dim,
+                                          name='state')
+            augmented_obs_ph = obs_ph
         with tf.compat.v1.variable_scope(self.name) as vs:
             self._variable_scope = vs
-            self.model.build(obs_ph, action_ph)
-
+            self.model.build(augmented_obs_ph, action_ph)
         self._f_qval = tf.compat.v1.get_default_session().make_callable(
             self.model.networks['default'].outputs,
             feed_list=[obs_ph, action_ph])
+        
+        self._obs_input = obs_ph
+        self._act_input = action_ph
 
     @property
     def inputs(self):
@@ -168,7 +174,7 @@ class ContinuousCNNQFunction(QFunction):
         tensor with shape :math:`(N, O*)`, and the second is the action tensor
         with shape :math:`(N, A*)`.
         """
-        return self.model.networks['default'].inputs
+        return self._obs_input, self._act_input
 
     def get_qval(self, observation, action):
         """Q Value of the network.
@@ -183,11 +189,10 @@ class ContinuousCNNQFunction(QFunction):
                 corresponding to each (obs, act) pair.
 
         """
-        if isinstance(self._env_spec.observation_space, akro.Image):
-            if len(observation.shape) <= 3:
-                observation = self._env_spec.observation_space.unflatten(
-                    observation)
-            observation = observation / 255.0
+        if len(observation.shape) < len(self._obs_dim):
+            observation = self._env_spec.observation_space.unflatten(
+                observation)
+        
         return self._f_qval(observation, action)
 
     # pylint: disable=arguments-differ
@@ -206,9 +211,10 @@ class ContinuousCNNQFunction(QFunction):
 
         """
         with tf.compat.v1.variable_scope(self._variable_scope):
+            augmented_state_input = state_input
             if isinstance(self._env_spec.observation_space, akro.Image):
-                state_input /= 255.0
-            return self.model.build(state_input, action_input, name=name)
+                augmented_state_input = tf.cast(state_input, tf.float32) / 255.0
+            return self.model.build(augmented_state_input, action_input, name=name)
 
     def clone(self, name):
         """Return a clone of the Q-function.
@@ -250,6 +256,8 @@ class ContinuousCNNQFunction(QFunction):
         """
         new_dict = self.__dict__.copy()
         del new_dict['_f_qval']
+        del new_dict['_obs_input']
+        del new_dict['_act_input']
         return new_dict
 
     def __setstate__(self, state):