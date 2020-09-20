647f964f495b1bcb9e3d0d452f216a800c9b809e
QIAOYI FANG <QF30@duke.edu>
2020-09-20 19:16:58 -0400

    skills is already in one hot

--- src/garage/torch/algos/kant.py
+++ src/garage/torch/algos/kant.py
@@ -275,9 +275,9 @@ class MetaKant(MetaRLAlgorithm):
             kl_loss = self._kl_lambda * kl_div
             kl_loss.backward(retain_graph=True)
 
-        print(skills)
-        print(skills.size())
-        skills_target = torch.eye(self._num_skills)[skills]
+        # print(skills)
+        # print(skills.size())
+        skills_target = skills
 
         policy_loss = F.mse_loss(skills_pred.flatten(), skills_target.flatten())\
                       * self._skills_reason_reward_scale
