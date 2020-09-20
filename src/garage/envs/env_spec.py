9c6aa70feae6812ac0893a5abbe24c9062984029
QIAOYI FANG <QF30@duke.edu>
2020-09-19 20:36:19 -0400

    changed env_spec

--- src/garage/envs/env_spec.py
+++ src/garage/envs/env_spec.py
@@ -13,7 +13,7 @@ class EnvSpec(InOutSpec):
     """
 
     def __init__(self, observation_space, action_space):
-        super().__init__(action_space, observation_space)
+        super().__init__(observation_space, action_space)
 
     @property
     def action_space(self):
@@ -23,7 +23,7 @@ class EnvSpec(InOutSpec):
             akro.Space: Action space of the env.
 
         """
-        return self.input_space
+        return self.output_space
 
     @property
     def observation_space(self):
@@ -33,7 +33,7 @@ class EnvSpec(InOutSpec):
             akro.Space: Observation space.
 
         """
-        return self.output_space
+        return self.input_space
 
     @action_space.setter
     def action_space(self, action_space):
@@ -43,7 +43,7 @@ class EnvSpec(InOutSpec):
             action_space (akro.Space): Action space.
 
         """
-        self._input_space = action_space
+        self._output_space = action_space
 
     @observation_space.setter
     def observation_space(self, observation_space):
@@ -53,4 +53,4 @@ class EnvSpec(InOutSpec):
             observation_space (akro.Space): Observation space.
 
         """
-        self._output_space = observation_space
+        self._input_space = observation_space
