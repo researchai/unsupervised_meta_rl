553d08f16a5901766ac884568177efae13f5f8cf
QIAOYI FANG <QF30@duke.edu>
2020-09-20 17:33:36 -0400

    changed diayn_pearl_half_cheetah_vel to loading

--- examples/torch/diayn_pearl_half_cheetah_vel.py
+++ examples/torch/diayn_pearl_half_cheetah_vel.py
@@ -104,17 +104,17 @@ def diayn_half_cheetah_vel_batch_for_pearl(ctxt=None, seed=1):
 
 
 s = np.random.randint(0, 1000)  # 521 in the sac_cheetah example
-task_proposer, diayn_trained_agent = diayn_half_cheetah_vel_batch_for_pearl(seed=s)
+# task_proposer, diayn_trained_agent = diayn_half_cheetah_vel_batch_for_pearl(seed=s)
 
 
-# load_dir = os.path.join(os.getcwd(), 'data/local/experiment/diayn_half_cheetah_vel_batch_for_pearl')
-# itr = 844
-# load_from_file = os.path.join(load_dir, 'itr_{}.pkl'.format(itr))
-# file = open(load_from_file, 'rb')
-# saved = joblib.load(file)
-# file.close()
-# diayn = saved['algo']
-# task_proposer = diayn.networks[1]  # _discriminator
+load_dir = os.path.join(os.getcwd(), 'data/local/experiment/diayn_half_cheetah_vel_batch_for_pearl')
+itr = 900
+load_from_file = os.path.join(load_dir, 'itr_{}.pkl'.format(itr))
+file = open(load_from_file, 'rb')
+saved = joblib.load(file)
+file.close()
+diayn = saved['algo']
+task_proposer = diayn.networks[1]  # _discriminator
 
 ########################## hyper params for PEARL ##########################
 
